bf1d72bdaac0cbfa729b118c0e828b5e34ccadb0
Moritz Wright <moritzwright@MacBookPro.fritz.box>
2025-01-12 22:01:09 +0100

    changes in readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # chatbot-admin-api
+
+### Start Virtual Environment:
+
+source venv/bin/activate
+
+### Start App:
+
+uvicorn app.main:app --reload
+
+### Test App:
+
+PYTHONPATH=$(pwd) pytest
--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 # Initialize database tables
 Base.metadata.create_all(bind=engine)
 
-# Include routers
-app.include_router(users_router, prefix="/api/users", tags=["users"])
-
 @app.get("/")
 async def root():
     return {"message": "Welcome Stranger!"}
+
+# Include routers
+app.include_router(users_router, prefix="/api/users", tags=["users"])
